68ba8438385f5d0334f4d1ad055ccb7a99a6c0ba
Adriano Rocha <adriano.olr@gmail.com>
2021-05-07 18:33:09 -0300

    Página home

---
+++ components/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from '../styles/components/Button.module.css';
+
+function Button({ children }) {
+  return (
+    <button className={styles.button}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
