9ae3536f60a3e0b34e6170217b3ea8a3857b17ad
Ryan Sales <sales@ufpi.edu.br>
2021-05-10 18:19:46 -0300

    Alterações na listagem de repositórios

--- pages/list.jsx
+++ pages/list.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { MdFileUpload } from 'react-icons/md';
+import { GoGear } from 'react-icons/go';
+import { BsThreeDotsVertical } from 'react-icons/bs'
 import Header from '../components/Header';
 
 import Button from '../components/Button';
@@ -12,12 +14,44 @@ function List() {
       <Header />
 
       <main className={styles.main}>
-        <div>
+        <div className={styles.containerOptions}>
           <Button>
             <MdFileUpload />
             <span>Enviar Modelo</span>
           </Button>
+          <Button>
+            <GoGear/>
+          </Button>
         </div>
+        <div className={styles.containerLabels}>
+          <div>
+            <a>Nome</a>
+          </div>
+          <div className={styles.containerLabels}>
+            <a className={styles.nameOptions}>Versão</a>
+            <a className={styles.nameOptions}>Tipo</a>
+            <a className={styles.nameOptions}>Métricas</a>
+            <a className={styles.nameOptions}>Framework</a>
+            <a className={styles.nameOptions}>Ações</a>
+          </div>
+        </div>
+        <div className={styles.containerItem}>
+          <div>
+            <a>modelo_1_exemplo.h5</a>
+          </div>
+          <div className={styles.containerItem}>
+            <a className={styles.nameOptions}>1.0</a>
+            <a className={styles.nameOptions}>Classificação</a>
+            <a className={styles.nameOptions}>90 acc</a>
+            <a className={styles.nameOptions}>Kensorflow</a>
+            <a className={styles.nameOptions}>
+              <Button>
+                <BsThreeDotsVertical />
+              </Button>
+            </a>
+          </div>
+        </div>
+        
       </main>
     </div>
   );
